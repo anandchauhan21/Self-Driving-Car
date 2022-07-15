64043001e987bb4f7e37287e20af4c1b462b148f
ANAND <anandchauhan8791@gmail.com>
2022-07-15 13:45:32 +0530

    📦 update some code

--- Trainingsim.py
+++ Trainingsim.py
@@ -1,5 +1,6 @@
 import utlis
 from utlis import *
+from sklearn.model_selection import train_test_split
 
 ### STEP 1
 path = 'myData'
@@ -12,3 +13,16 @@ data = balanceData(data, display=False)
 ### STEP 3
 imagesPath, steering = utlis.loadDate(path, data)
 print(imagesPath[0], steering[0])
+
+### STEP 4
+xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steering, test_size=0.2, random_state=5)
+print('Total Training Image: ', len(xTrain))
+print('Total Validation Image: ', len(xVal))
+
+### STEP 5
+
+### STEP 6
+
+### STEP 7
+
+### STEP 8
--- utlis.py
+++ utlis.py
@@ -1,8 +1,17 @@
+import random
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
 from sklearn.utils import shuffle
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
+import cv2
+
+from keras.models import Sequential
+from keras.layers import Convolution2D,Flatten,Dense
+from keras.optimizers import Adam
 
 
 def getName(filePath):
@@ -60,3 +69,64 @@ def loadDate(path, data):
     imagePath = np.asarray(imagePath)
     steering = np.asarray(steering)
     return imagePath, steering
+
+def augmentImage(imgPath, steering):
+    img = mpimg.imread(imgPath)
+
+    ## PEN
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={'x':(-0.1,0.1),'y':(-0.1,0.1)})
+        img = pan.augment_image(img)
+
+    ## ZOOM
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1,1.2))
+        img = zoom.augment_image(img)
+
+    ## BRIGHTNESS
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.4,1.2))
+        img = brightness.augment_image(img)
+
+    ## FLIP
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img,1)
+        steering = -steering
+
+
+    return img,steering
+
+"""
+imgRE,st = augmentImage('test.jpg',0)
+plt.imshow(imgRE)
+plt.show()
+"""
+
+def preProcessing(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img,cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,(3,3),0)
+    img = cv2.resize(img,(200,66))
+    img = img /255
+    return img
+
+imgRE = preProcessing(mpimg.imread('test.jpg'))
+plt.imshow(imgRE)
+plt.show()
+
+def batchGen(imagesPath, steeringList,batchSize, trainFlag):
+    while True:
+        imgBatch = []
+        steeringBatch = []
+
+        for i in range(batchSize):
+            index = random.randint(0, len(imagesPath)-1)
+            if trainFlag:
+                img, steering = augmentImage(imagesPath[index], steeringList[index])
+            else:
+                img = mpimg.imread(imagesPath[index])
+                steering = steeringList[index]
+            img = preProcessing(img)
+            imgBatch.append(img)
+            steeringBatch.append(steering)
+        yield (np.asarray(imgBatch),np.asarray(steeringBatch))
